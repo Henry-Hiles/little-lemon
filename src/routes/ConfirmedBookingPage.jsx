562b2a4a308f126076716a269322ee98dec14cc2
Henry Hiles <henry@henryhiles.com>
2023-08-17 19:53:36 -0400

    General improvements

--- src/routes/ConfirmedBookingPage.jsx
+++ src/routes/ConfirmedBookingPage.jsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom"
 import "../styles/ConfirmedBookingPage.css"
 
 export const ConfirmedBookingPage = () => (
-	<article className="confirmed">
-		<h1>Booking Confirmed</h1>
-		<p>
-			Your booking has been confirmed. <Link to="/">Back to home</Link>
-		</p>
-	</article>
+	<section className="confirmed">
+		<article>
+			<h1>Booking Confirmed</h1>
+			<p>
+				Your booking has been confirmed.{" "}
+				<Link to="/">Back to home</Link>
+			</p>
+		</article>
+	</section>
 )
